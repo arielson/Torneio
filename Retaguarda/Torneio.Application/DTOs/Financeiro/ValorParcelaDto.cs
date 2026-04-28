a1bb3967b5fcbb9f744a7b6108d257ae811a00c1
Arielson Ferreira <arielsonf@hotmail.com>
2026-04-28 06:37:28 -0300

    Ajustes

--- Retaguarda/Torneio.Application/DTOs/Financeiro/ValorParcelaDto.cs
+++ Retaguarda/Torneio.Application/DTOs/Financeiro/ValorParcelaDto.cs
@@ -2,6 +2,6 @@ namespace Torneio.Application.DTOs.Financeiro;
 
 public class ValorParcelaDto
 {
-    public int NumeroParcela { get; init; }
-    public decimal Valor { get; init; }
+    public int NumeroParcela { get; set; }
+    public decimal Valor { get; set; }
 }
